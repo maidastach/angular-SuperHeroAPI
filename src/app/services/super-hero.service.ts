20261a2198f03eb64567ee98de80ca471969ddde
Salvatore De Rosa <Salvatore De Rosa@XPS-7590>
2022-12-26 20:38:51 +1100

    created services

---
+++ src/app/services/super-hero.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { SuperHero } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SuperHeroService {
+  private url = 'SuperHero';
+  constructor(private http: HttpClient) {}
+
+  getSuperHeroes(): Observable<SuperHero[]> {
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+  }
+  updateSuperHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+  createSuperHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(`${environment.apiUrl}/${this.url}`, hero);
+  }
+  deleteSuperHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/${hero.id}`);
+  }
+}
