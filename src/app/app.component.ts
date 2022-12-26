5f643d03625c2309c3e67dec1b339d955c62302c
Salvatore De Rosa <Salvatore De Rosa@XPS-7590>
2022-12-26 20:39:39 +1100

    finishing app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DerivcoPrepAng';
+  heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
+
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnInit(): void {
+    this.superHeroService
+      .getSuperHeroes()
+      .subscribe((result: SuperHero[]) => (this.heroes = result));
+    console.log(this.heroes);
+  }
+
+  setHeroToEdit(hero: SuperHero): void {
+    this.heroToEdit = hero;
+  }
+
+  createHero(): void {
+    this.heroToEdit = new SuperHero() 
+  }
+
+  updateHeroList(heroes: SuperHero[]): void {
+    this.heroes = heroes
+    this.heroToEdit = undefined
+  }
 }
